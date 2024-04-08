36f291cfb19dfc0d613edc46f253d487a293116e
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2024-04-08 17:37:37 +0200

    + css sur page inscription et connexion

--- site/Con+Ins/connexion.php
+++ site/Con+Ins/connexion.php
@@ -1,10 +1,10 @@
-    <!-- connexion.php -->
+  <!-- connexion.php -->
 
 
-    <?php
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+  <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
 
     require_once("../database/database.php");
@@ -35,37 +35,88 @@
     }
     ?>
 
-    <!DOCTYPE html>
-    <html lang="fr">
 
-    <head>
-        <link rel="stylesheet" type="text/css" href="../style/styles.css">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Page de connexion</title>
-    </head>
+  <!DOCTYPE html>
+  <html lang="fr">
 
-    <body>
-        <div class="form-container">
-            <h1 class="form-title">Connectez-vous</h1>
-            <form action="" method="post" class="form">
-                <input type="text" name="nom" placeholder="Nom" required>
-                <input type="password" name="mot_de_passe" placeholder="Mot de passe" required>
-                <input type="submit" name="connexion" value="Envoyer">
-                <?php if (isset($error_message)) { ?>
-                    <p class="error-message"><?php echo $error_message; ?></p>
-                <?php } ?>
-            </form>
-            <div class="form-separator">
-                <strong>OU</strong>
-                <hr class="separator">
-            </div>
-            <div class="signin-message">
-                <span>Pas encore de compte ?</span>
-                <a href="inscription.php">Inscrivez-vous.</a>
-            </div>
-        </div>
+  <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta name="description" content="" />
+      <meta name="author" content="" />
+      <title>Creative - Start Bootstrap Theme</title>
+      <!-- Favicon-->
+      <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      <!-- Bootstrap Icons-->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+      <!-- Google fonts-->
+      <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+      <!-- SimpleLightbox plugin CSS-->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+      <!-- Core theme CSS (includes Bootstrap)-->
+      <link href="../css/styles.css" rel="stylesheet" />
+      <link rel="stylesheet" type="text/css" href="../styleFormulaire/styles.css">
 
-    </body>
+  </head>
 
-    </html>
+  <body id="page-top">
+      <!-- Navigation-->
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+          <div class="container px-4 px-lg-5">
+              <a class="navbar-brand" href="#page-top">Barriere</a>
+              <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+              <div class="collapse navbar-collapse" id="navbarResponsive">
+                  <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                      <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                      <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+                      <li class="nav-item"><a class="nav-link" href="Con+Ins/inscription.php">Inscription</a></li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
+      <!-- Masthead-->
+      <header class="masthead">
+          <div class="form-container">
+              <h1 class="form-title">Connectez-vous</h1>
+              <form action="" method="post" class="form">
+                  <input type="text" name="nom" placeholder="Nom" required>
+                  <input type="password" name="mot_de_passe" placeholder="Mot de passe" required>
+                  <input type="submit" name="connexion" value="Envoyer">
+                  <?php if (isset($error_message)) { ?>
+                      <p class="error-message"><?php echo $error_message; ?></p>
+                  <?php } ?>
+              </form>
+              <div class="form-separator">
+                  <strong>OU</strong>
+                  <hr class="separator">
+              </div>
+              <div class="signin-message">
+                  <span>Pas encore de compte ?</span>
+                  <a href="inscription.php">Inscrivez-vous.</a>
+              </div>
+          </div>
+
+      </header>
+
+
+      <!-- Footer-->
+      <footer class="bg-light py-5">
+          <div class="container px-4 px-lg-5">
+              <div class="small text-center text-muted">Copyright &copy; 2023 - Company Name</div>
+          </div>
+      </footer>
+      <!-- Bootstrap core JS-->
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+      <!-- SimpleLightbox plugin JS-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+      <!-- Core theme JS-->
+      <script src="../js/scripts.js"></script>
+      <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+      <!-- * *                               SB Forms JS                               * *-->
+      <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+      <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+      <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+  </body>
+
+  </html>
--- site/Con+Ins/inscription.php
+++ site/Con+Ins/inscription.php
@@ -32,35 +32,83 @@ if (isset($_POST['inscription'])) {
 <html lang="fr">
 
 <head>
-    <link rel="stylesheet" type="text/css" href="../style/styles.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page d'inscription</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Creative - Start Bootstrap Theme</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap Icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+    <!-- SimpleLightbox plugin CSS-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../css/styles.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="../styleFormulaire/styles.css">
+
 </head>
 
-<body>
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#page-top">Barriere</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+                    <li class="nav-item"><a class="nav-link" href="Con+Ins/inscription.php">Inscription</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead">
+        <div class="form-container">
+            <h1 class="form-title">Inscrivez-vous</h1>
+            <form action="" method="post" class="form">
+                <input type="text" name="login" placeholder="Nom d utilisateur" required>
+                <input type="password" name="password" placeholder="Mot de passe" required>
+                <input type="email" name="mail" placeholder="Adresse e-mail" required>
+                <input type="submit" name="inscription" value="S inscrire">
+            </form>
+            <div class="form-separator">
+                <strong>OU</strong>
+                <hr class="separator">
+            </div>
+            <div class="signin-message">
+                <span>Déjà un compte ?</span>
+                <a href="connexion.php">Connectez-vous.</a>
+            </div>
+        </div>
 
-<div class="form-container">
-    <h1 class="form-title">Inscrivez-vous</h1>
-    <form action="" method="post" class="form">
-        <input type="text" name="login" placeholder="Nom d utilisateur" required>
-        <input type="password" name="password" placeholder="Mot de passe" required>
-        <input type="email" name="mail" placeholder="Adresse e-mail" required>
-        <input type="submit" name="inscription" value="S inscrire">
-    </form>
-    <div class="form-separator">
-        <strong>OU</strong>
-        <hr class="separator">
-    </div>
-    <div class="signin-message">
-        <span>Déjà un compte ?</span>
-        <a href="connexion.php">Connectez-vous.</a>
-    </div>
-</div>
+
+        </div>
+    </header>
 
 
-    </div>
-
+    <!-- Footer-->
+    <footer class="bg-light py-5">
+        <div class="container px-4 px-lg-5">
+            <div class="small text-center text-muted">Copyright &copy; 2023 - Company Name</div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- SimpleLightbox plugin JS-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="../js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 </body>
 
 </html>

2823ed4b5f7249f8dfa2ed29ffd2503b766e40d0
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2024-04-09 10:55:30 +0200

    maj navbar + footer

--- site/Con+Ins/connexion.php
+++ site/Con+Ins/connexion.php
@@ -68,16 +68,15 @@
               <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
               <div class="collapse navbar-collapse" id="navbarResponsive">
                   <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                      <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                      <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
-                      <li class="nav-item"><a class="nav-link" href="inscription.php">Inscription</a></li>
+                       <li class="nav-item"><a class="nav-link" href="../index.html">Accueil</a></li>
+                       <li class="nav-item"><a class="nav-link" href="inscription.php">Inscription</a></li>
                   </ul>
               </div>
           </div>
       </nav>
       <!-- Masthead-->
       <header class="masthead">
-          <div class="form-container">
+          <div class="form-container"> 
               <h1 class="form-title">Connectez-vous</h1>
               <form action="" method="post" class="form">
                   <input type="text" name="nom" placeholder="Nom" required>

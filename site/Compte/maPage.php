c2b001ba43df935144544a6ce9f027ce72a2efc0
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2024-05-24 15:19:12 +0200

    maj maPage.php

--- site/Compte/maPage.php
+++ site/Compte/maPage.php
@@ -46,7 +46,7 @@ if (!empty($demande_ids)) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Creative - Start Bootstrap Theme</title>
+    <title>Ma page</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Bootstrap Icons-->
@@ -65,15 +65,15 @@ if (!empty($demande_ids)) {
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
         <div class="container px-4 px-lg-5">
-            <a class="navbar-brand" href="#page-top">Barriere</a>
+            <a class="navbar-brand" href="../index.html">Barriere</a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="#about">A propos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="Con+Ins/inscription.php">Inscription</a></li>
-                    <li class="nav-item"><a class="nav-link" href="module_de_test/indexModule.html">Module de test</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../index.html">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../index.html#about">A propos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="../Con+Ins/inscription.php">Inscription</a></li>
                 </ul>
             </div>
         </div>
